d207917b80fd497c5f642ad364f111f89770dc36
praveen-gitdev <praveenpharmaten@gmail.com>
2024-12-04 23:20:42 +0530

    Update and rename test.py to test01.csv

--- test.py
+++
@@ -1,9 +0,0 @@
-
-import requests
-
-def fetch_exchange_metadata(api_url):
-    response = requests.get(api_url)
-    if response.status_code == 200:
-        return response.json()
-    else:
-        raise Exception("Failed to fetch metadata")
